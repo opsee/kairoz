ed5edd745df91a0bd637a1e1eb4323a76e109892
Dan Compton <dan@opsee.com>
2016-07-18 15:38:56 -0400

    Update ELB resource

--- terraform/kairoz.tf
+++ terraform/kairoz.tf
@@ -21,4 +21,11 @@ resource "aws_ecs_service" "kairoz" {
     desired_count = 2
     deployment_maximum_percent = 200
     deployment_minimum_healthy_percent = 100
+    iam_role = "${var.ecs_iam_role}"
+
+    load_balancer {
+        elb_name = "kairoz"
+        container_name = "kairoz"
+        container_port = 9113
+    }
 }
